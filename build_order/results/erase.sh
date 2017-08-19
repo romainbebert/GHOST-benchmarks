0e7982d6419e0e1fa5f1fa9e2ff00fe10412895d
Florian Richoux <florian.richoux@polytechnique.edu>
2017-08-19 11:17:23 +0200

    Remove other build order

--- build_order/results/erase.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in $(ls -l $1 | grep -E " 10 ")
-do
-    if [[ $i == *short* ]]
-    then
-	rm -f $1/$i
-    fi
-done
--- build_order/tuning_parameters/analyse.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in $(ls bo*)
-do
-    echo $i
-    ./analyse.rb $i
-    echo 
-done
