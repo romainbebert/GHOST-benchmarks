fac6d5fce7216080c096dc7906e16d9f77593f39
Florian Richoux <florian.richoux@polytechnique.edu>
2017-08-31 04:35:00 +0200

    New runs and script

--- resource/scripts/analyse_results.rb
+++ resource/scripts/analyse_results.rb
@@ -13,8 +13,11 @@ end
 file = File.open(ARGV[0])
 
 total = 0
+success = 0
 dps = 0
 max = 0
+satloop = 0
+optiloop = 0
 
 if ARGV[0].include? "zerg"
   opti = "11400"
@@ -26,25 +29,41 @@ else
   puts "Bad file name"
   exit
 end
-  
 
+puts opti
   
 # For each line in file
 file.each do |line|
-  words = line.split(' : ')
-  if words[0].include? "DPS"
-    if words[1].include? opti
-      max += 1
+  words = line.split(':')
+  if not words[0] == nil
+    if words[0].include? "Satisfaction cost"
+      total += 1
+      if words[1].to_f == 0
+        success += 1
+      end
+    elsif words[0].include? "Optimization cost"
+      if words[1].to_f == opti.to_f
+        max += 1
+      end
+      dps += words[1].to_i
+    elsif words[0].include? "Number of optization loops"
+      optiloop += words[1].to_i
+    elsif words[0].include? "Number of satisfaction loops"
+      satloop += words[1].to_i
     end
-    dps += words[1].to_i
-    total += 1
   end
 end
 
 mean_dps = (dps.to_f / total).round(1)
+mean_satloop = (satloop.to_f / total).round(1)
+mean_optiloop = (optiloop.to_f / total).round(1)
 percent = (100 * max.to_f / total).round(1)
+percent_success = (100 * success.to_f / total).round(1)
 
 puts "#{ARGV[0]}:\n   #{percent}\% of perfects (#{max}/#{total})"
-puts "   Average dps = #{mean_dps}"
+puts "   #{percent_success}\% of success (#{success}/#{total})\n"
+puts "   Average dps = #{mean_dps}\n"
+puts "   Average sat loop = #{mean_satloop}\n"
+puts "   Average opt loop = #{mean_optiloop}\n"
 
 exit
--- resource/src/resource_problem.cpp
+++ resource/src/resource_problem.cpp
@@ -72,160 +72,157 @@ int main(int argc, char **argv)
   // Define objective
   shared_ptr<MaxGroundDPS> objective = make_shared<MaxGroundDPS>();
 
+  string race = (string)argv[1];
+  
   ////////////
   // Terran //
   ////////////
-  
-  // Create variables
-  auto variables_t = make_terran( 380 );    
-
-  // Define constraints 
-  Stock mineral_t( &variables_t, 20000, ResourceType::Mineral );
-  Stock gas_t( &variables_t, 14000, ResourceType::Gas );
-  Stock supply_t( &variables_t, 380, ResourceType::Supply );
-  
-  vector< Stock > constraints_t { mineral_t, gas_t, supply_t };
-
-  // Define solver
-  Solver<Unit, Stock> solver_t( variables_t,
-  				constraints_t,
-  				objective );
-
-  double cost_t = 0.;
-  vector<int> solution_t( variables_t.size(), 0 );
-
-  // bool found_t = solver_t.solve( cost_t, solution_t, 20, 130 );
-  // bool found = solver_t.solve( cost_t, solution_t, 100, 1500 );
-  // bool found = solver_t.solve( cost_t, solution_t, 1000, 15000 );
-
-  int count = 0;
-  double total = 0.;
-  
-  for(int i = 0 ; i < 100 ; ++i )
-    if( solver_t.solve( cost_t, solution_t, 20, 130 ) )
-    {
-      ++count;
-      total += cost_t;
-    }
-
-  // process_mem_usage(vm, rss);
-  
-  cout << "*** Terran ***\n"
-       << "Success rate: " << count << "%\n"
-       << "Mean score: " << total/count << "\n";
-  //<< "Memory usage: " << rss << "/" << vm << "\n\n";
-  
-  // cout << "var-------\n";
-  // for( auto& v : variables_t )
-  //   cout << v.get_name() << ":" << v.get_value() << "\n";
-  // cout << "sol-------\n";
-  // for( auto& v : variables_t )
-  //   cout << v.get_name() << ":" << solution_t[ v.get_id() ] << "\n";
-  
-  cout << "\n\n";
+  if( race == "terran" )
+  {
+    // Create variables
+    auto variables_t = make_terran( 380 );    
     
+    // Define constraints 
+    Stock mineral_t( &variables_t, 20000, ResourceType::Mineral );
+    Stock gas_t( &variables_t, 14000, ResourceType::Gas );
+    Stock supply_t( &variables_t, 380, ResourceType::Supply );
+    
+    vector< Stock > constraints_t { mineral_t, gas_t, supply_t };
+    
+    // Define solver
+    Solver<Unit, Stock> solver_t( variables_t,
+				  constraints_t,
+				  objective );
+    
+    double cost_t = 0.;
+    vector<int> solution_t( variables_t.size(), 0 );
+    
+    int count = 0;
+    double total = 0.;
+    
+    for(int i = 0 ; i < 100 ; ++i )
+      if( solver_t.solve( cost_t, solution_t, 2, 130 ) )
+      {
+	++count;
+	total += cost_t;
+      }
+    
+    // process_mem_usage(vm, rss);
+    
+    // cout << "*** Terran ***\n"
+    //      << "Success rate: " << count << "%\n"
+    //      << "Mean score: " << total/count << "\n";
+    //<< "Memory usage: " << rss << "/" << vm << "\n\n";
+    
+    // cout << "var-------\n";
+    // for( auto& v : variables_t )
+    //   cout << v.get_name() << ":" << v.get_value() << "\n";
+    // cout << "sol-------\n";
+    // for( auto& v : variables_t )
+    //   cout << v.get_name() << ":" << solution_t[ v.get_id() ] << "\n";
+    
+    // cout << "\n\n";
+  }
+  
   /////////////
   // Protoss //
   /////////////
   
-  // Create variables
-  auto variables_p = make_protoss( 380 );    
+  if( race == "protoss" )
+  {
+    // Create variables
+    auto variables_p = make_protoss( 380 );    
 
-  // Define constraints 
-  Stock mineral_p( &variables_p, 20000, ResourceType::Mineral );
-  Stock gas_p( &variables_p, 14000, ResourceType::Gas );
-  Stock supply_p( &variables_p, 380, ResourceType::Supply );
+    // Define constraints 
+    Stock mineral_p( &variables_p, 20000, ResourceType::Mineral );
+    Stock gas_p( &variables_p, 14000, ResourceType::Gas );
+    Stock supply_p( &variables_p, 380, ResourceType::Supply );
   
-  vector< Stock > constraints_p { mineral_p, gas_p, supply_p };
+    vector< Stock > constraints_p { mineral_p, gas_p, supply_p };
 
-  // Define solver
-  Solver<Unit, Stock> solver_p( variables_p,
-  				constraints_p,
-  				objective );
+    // Define solver
+    Solver<Unit, Stock> solver_p( variables_p,
+				  constraints_p,
+				  objective );
 
-  double cost_p = 0.;
-  vector<int> solution_p( variables_p.size(), 0 );
+    double cost_p = 0.;
+    vector<int> solution_p( variables_p.size(), 0 );
   
-  // bool found_p = solver_p.solve( cost_p, solution_p, 20, 130 );
-  // bool found = solver_p.solve( cost_p, solution_p, 100, 1500 );
-  // bool found = solver_p.solve( cost_p, solution_p, 1000, 15000 );
-
-  count = 0;
-  total = 0.;
+    int count = 0;
+    double total = 0.;
   
-  for(int i = 0 ; i < 100 ; ++i )
-    if( solver_p.solve( cost_p, solution_p, 20, 130 ) )
-    {
-      ++count;
-      total += cost_p;
-    }
+    for(int i = 0 ; i < 100 ; ++i )
+      if( solver_p.solve( cost_p, solution_p, 2, 130 ) )
+      {
+	++count;
+	total += cost_p;
+      }
 
-  // process_mem_usage(vm, rss);
+    // process_mem_usage(vm, rss);
   
-  cout << "*** Protoss ***\n"
-       << "Success rate: " << count << "%\n"
-       << "Mean score: " << total/count << "\n";
+    // cout << "*** Protoss ***\n"
+    //      << "Success rate: " << count << "%\n"
+    //      << "Mean score: " << total/count << "\n";
     //<< "Memory usage: " << rss << "/" << vm << "\n\n";
 
-  // cout << "var-------\n";
-  // for( auto& v : variables_p )
-  //   cout << v.get_name() << ":" << v.get_value() << "\n";
-  // cout << "sol-------\n";
-  // for( auto& v : variables_p )
-  //     cout << v.get_name() << ":" << solution_p[ v.get_id() ] << "\n";
+    // cout << "var-------\n";
+    // for( auto& v : variables_p )
+    //   cout << v.get_name() << ":" << v.get_value() << "\n";
+    // cout << "sol-------\n";
+    // for( auto& v : variables_p )
+    //     cout << v.get_name() << ":" << solution_p[ v.get_id() ] << "\n";
 
-  cout << "\n\n";
-
+    // cout << "\n\n";
+  }
+  
   //////////
   // Zerg //
   //////////
+
+  if( race == "zerg" )
+  {
+    // Create variables
+    auto variables_z = make_zerg( 380 );    
+
+    // Define constraints 
+    Stock mineral_z( &variables_z, 20000, ResourceType::Mineral );
+    Stock gas_z( &variables_z, 14000, ResourceType::Gas );
+    Stock supply_z( &variables_z, 380, ResourceType::Supply );
   
-  // Create variables
-  auto variables_z = make_zerg( 380 );    
+    vector< Stock > constraints_z { mineral_z, gas_z, supply_z };
 
-  // Define constraints 
-  Stock mineral_z( &variables_z, 20000, ResourceType::Mineral );
-  Stock gas_z( &variables_z, 14000, ResourceType::Gas );
-  Stock supply_z( &variables_z, 380, ResourceType::Supply );
+    // Define solver
+    Solver<Unit, Stock> solver_z( variables_z,
+				  constraints_z,
+				  objective );
+
+    double cost_z = 0.;
+    vector<int> solution_z( variables_z.size(), 0 );
   
-  vector< Stock > constraints_z { mineral_z, gas_z, supply_z };
-
-  // Define solver
-  Solver<Unit, Stock> solver_z( variables_z,
-  				constraints_z,
-  				objective );
-
-  double cost_z = 0.;
-  vector<int> solution_z( variables_z.size(), 0 );
+    int count = 0;
+    double total = 0.;
   
-  // bool found_z = solver_z.solve( cost_z, solution_z, 20, 130 );
-  // bool found = solver_z.solve( cost_z, solution_z, 100, 1500 );
-  // bool found = solver_z.solve( cost_z, solution_z, 1000, 15000 );
+    for(int i = 0 ; i < 100 ; ++i )
+      if( solver_z.solve( cost_z, solution_z, 2, 130 ) )
+      {
+	++count;
+	total += cost_z;
+      }
 
-  count = 0;
-  total = 0.;
-  
-  for(int i = 0 ; i < 100 ; ++i )
-    if( solver_z.solve( cost_z, solution_z, 20, 130 ) )
-    {
-      ++count;
-      total += cost_z;
-    }
+    // process_mem_usage(vm, rss);
 
-  // process_mem_usage(vm, rss);
-
-  cout << "*** Zerg ***\n"
-       << "Success rate: " << count << "%\n"
-       << "Mean score: " << total/count << "\n";
+    // cout << "*** Zerg ***\n"
+    //      << "Success rate: " << count << "%\n"
+    //      << "Mean score: " << total/count << "\n";
     //<< "Memory usage: " << rss << "/" << vm << "\n\n";
 
-  // cout << "var-------\n";
-  // for( auto& v : variables_z )
-  //   cout << v.get_name() << ":" << v.get_value() << "\n";
-  // cout << "sol-------\n";
-  // for( auto& v : variables_z )
-  //   cout << v.get_name() << ":" << solution_z[ v.get_id() ] << "\n";
+    // cout << "var-------\n";
+    // for( auto& v : variables_z )
+    //   cout << v.get_name() << ":" << v.get_value() << "\n";
+    // cout << "sol-------\n";
+    // for( auto& v : variables_z )
+    //   cout << v.get_name() << ":" << solution_z[ v.get_id() ] << "\n";
 
-  cout << "\n\n";
+    // cout << "\n\n";
+  }
 }
-
